e02820aa5ae5218415191d681cbbc13c7f95ebd4
Zeeshan-Qbatch <zeeshan.ahmed@qbatch.com>
2019-08-01 18:17:16 +0500

    all api are written

--- server/models/items.js
+++ server/models/items.js
@@ -1,13 +1,12 @@
-import mongoose from 'mongoose';
+const mongoose=require('mongoose');
 
 const item = new mongoose.Schema({
   name: { type: String },
   price: { type: Number },
   details: { type: String },
   image: { type: String },
-  
   created : {type : Date},
   updated : {type : Date}
 });
 
-export default mongoose.model('item' , item );
+module.exports = mongoose.model('item' , item);
--- server/models/orders.js
+++ server/models/orders.js
@@ -1,11 +1,24 @@
-import mongoose from 'mongoose';
-
-const order = new mongoose.Schema({
+const mongoose=require('mongoose');
+var Schema= mongoose.Schema;
+const order = new Schema({
   status: {type:String,required: true },
   Total_Price : {type: Number},
   completed_Time: { type: Date},
+  customerId: {type: Schema.Types.ObjectId},
   created : {type : Date},
   updated : {type : Date}
 });
 
-export default mongoose.model('order', order);
+module.exports = mongoose.model('order' , order);
+// const mongoose=require('mongoose');
+
+// const order = new mongoose.Schema({
+//   status: {type:String,required: true },
+//   Total_Price : {type: Number},
+//   completed_Time: { type: Date},
+//   customerId: {type: Schema.Types.ObjectId},
+//   created : {type : Date},
+//   updated : {type : Date}
+// });
+
+// module.exports = mongoose.model('order' , order);
